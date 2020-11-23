7688c7c5934a55e689fbcb94203f5448038daa1a
lakshyagarg911 <lakshyagarg911@gmail.com>
2020-11-23 15:35:12 +0530

    Added Hover Effect and a play button

--- imports/extra_screens.py
+++ imports/extra_screens.py
@@ -1,23 +1,51 @@
 from .settings import *
 
+mouse_rect = pygame.Rect(0, 0, 10, 10)
+select_rect = pygame.Rect(0, 0, 0, 0)
+select_rect_color = GRAY
+
+def hover(obj_rect, Screen):
+    mouse_rect.center = pygame.mouse.get_pos()
+    if mouse_rect.colliderect(obj_rect):
+        select_rect.topleft = obj_rect.topleft
+        select_rect.size = obj_rect.size
+
+        ## Not using pygame.draw.rect cuz usme opacity set nhi hoti
+        s_img = pygame.Surface(select_rect.size)
+        s_img.set_alpha(50)
+        s_img.fill(select_rect_color)
+        Screen.blit(s_img, select_rect.topleft)
+
+
 def welcome_screen(screen):
-    clicked = False
-    while True:
-        screen.fill((255,255,255))
+		clicked = False
+		while True:
+				screen.fill((255,255,255))
 
-        # display 
-        heading_text = big_font.render('Clicker Ball!', True, (0, 0, 0))
-        heading_rect = heading_text.get_rect()
-        heading_rect.center = (WW/2, 50)
-        screen.blit(heading_text, heading_rect.topleft)
+				# display 
+				heading_text = big_font.render('Clicker Ball!', True, (0, 0, 0))
+				heading_rect = heading_text.get_rect()
+				heading_rect.center = (WW/2, 50)
+				screen.blit(heading_text, heading_rect.topleft)
 
-       	pygame.display.update()
+				heading_text = big_font.render('Play', True, (0, 0, 0))
+				heading_rect = heading_text.get_rect()
+				heading_rect.center = (WW/2, 250)
+				screen.blit(heading_text, heading_rect.topleft)
 
-       	# Events
-       	for event in pygame.event.get():
-       		if event.type == pygame.QUIT:
-       			return 'quit'
-       		elif event.type == pygame.KEYDOWN:
-       			return 'start'
-       		elif event.type == pygame.MOUSEBUTTONDOWN:
-       			clicked = True
+				hover(heading_rect, screen)
+				if clicked:
+					if heading_rect.left < mx < heading_rect.right and heading_rect.top < my < heading_rect.bottom:
+						return 'start'
+
+
+				# Events
+				clicked = False
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+						return 'quit'
+					elif event.type == pygame.MOUSEBUTTONDOWN:
+						clicked = True
+						mx,my = pygame.mouse.get_pos()
+
+				pygame.display.update()
