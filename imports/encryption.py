62790017f91c07a472c2eb2f3a87001ffebf3c62
lakshyagarg911 <lakshyagarg911@gmail.com>
2020-11-29 15:30:44 +0530

    Encryption Algorithm Changed

--- imports/encryption.py
+++ imports/encryption.py
@@ -1,32 +1,13 @@
+from cryptography.fernet import *
+
+
 class Crypt:
-    key = "}>CX[JAnpm8)H[rKEvrt/kse1G'j{Pd\jxfTNCxU/b4i0MeeV9A(FusO9zd9bM\m"
-    splitter = "z"
+    key = b'E0kGTj9oe68mgUnNZuMwfzosAWO4C1YiO_EIBsaQcTw='
 
     @staticmethod
-    def de(inputed_str):
-        decrypted = ""
-        encrypted_list = inputed_str[:-1].split(Crypt.splitter)
-
-        index = 0
-        for num in encrypted_list:
-            try:
-                decrypted += chr(int(int(num) / ord(Crypt.key[index])))
-            except IndexError:
-                decrypted += chr(int(int(num) / ord(Crypt.key[index % 64])))
-            finally:
-                index += 1
-
-        return decrypted
+    def de(input_str):
+        return Fernet(Crypt.key).decrypt(input_str.encode()).decode()
 
     @staticmethod
-    def en(string):
-        index = 0
-        encrypted = ""
-        for alpha in string:
-            try:
-                encrypted += str(ord(alpha) * ord(Crypt.key[index])) + Crypt.splitter
-            except IndexError:
-                encrypted += str(ord(alpha) * ord(Crypt.key[index % 64])) + Crypt.splitter
-            finally:
-                index += 1
-        return encrypted
+    def en(input_string):
+        return Fernet(Crypt.key).encrypt(input_string.encode()).decode()
